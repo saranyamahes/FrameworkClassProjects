a90eab281c935a5a85ebd7e4f87de2f972033995
saranyamahes <saaranselvi@gmail.com>
2024-11-24 12:48:19 +0530

    code updation

--- src/test/java/Test_A.java
+++ src/test/java/Test_A.java
@@ -4,11 +4,14 @@ public class Test_A {
 	public void method1() {
 		System.out.println("Method 1");	
 		}
+	public void method2() {
+		System.out.println("Method 2");
+	}
 	
 	public static void main(String[] args) {
 	
-Test_A t=new Test_A();
-t.method1();
+		Test_A t=new Test_A();
+		t.method1();
 		
 	}
 
