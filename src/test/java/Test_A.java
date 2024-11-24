66bedf41cdadbc8aa82b7d7a15cc01942b565673
saranyamahes <125350447+saranyamahes@users.noreply.github.com>
2024-11-24 13:03:03 +0530

    Update Test_A.java

--- src/test/java/Test_A.java
+++ src/test/java/Test_A.java
@@ -4,7 +4,7 @@ public class Test_A {
 	public void method1() {
 		System.out.println("Method 1");	
 		}
-	public class method2(){
+	public class method3(){
 		System.out.println("Method 3");
 	}
 	public static void main(String[] args) {
