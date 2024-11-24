593c3e0dac2132db7e3569e57e588f4ea251c62e
saranyamahes <saaranselvi@gmail.com>
2024-11-24 11:49:55 +0530

    My project code

---
+++ src/test/java/Test_A.java
@@ -0,0 +1,15 @@
+
+public class Test_A {
+
+	public void method1() {
+		System.out.println("Method 1");	
+		}
+	
+	public static void main(String[] args) {
+	
+Test_A t=new Test_A();
+t.method1();
+		
+	}
+
+}
